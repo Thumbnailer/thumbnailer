4ff3efea0e8d7e5dcc0458be80650c16f1ddc4ce
Samuel Dieterle <samuel@dieterle.io>
2020-05-04 15:29:18 +0200

    Added Thumbnail struct and Opertions scaffolding.
    The Thumbnail struct is still WIP.
    For every operation on an image there is now a struct which contains the
    arugments for that operation and a apply(image) method, which applies
    that operation onto the given image.

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -1,5 +1,9 @@
 use image::DynamicImage;
 use crate::generic::GenericThumbnail;
+use std::path::Path;
+use crate::thumbnail::operations::Operation;
+
+mod operations;
 
 pub struct StaticThumbnail {
     image: DynamicImage,
@@ -10,4 +14,17 @@ impl StaticThumbnail {
         &self.image
     }
 }
+
+pub trait SingleThumbnail : GenericThumbnail {
+    fn as_static_copy(&self) -> &mut StaticThumbnail;
+}
+
+pub struct Thumbnail<'a> {
+    path: &'a Path,
+    height: u32,
+    width: u32,
+    image: Option<DynamicImage>,
+    ops: Vec<&'a dyn Operation>
+}
+
 
