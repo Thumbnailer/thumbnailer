09df98eaa4ba76f5fc399846492f6539e52e941a
Samuel Dieterle <samuel@dieterle.io>
2020-05-06 13:16:58 +0200

    Implemented Thumbnail::load and Thumbnail::can_load
    ::load constructs a Thumbnail form the given path instance and returns it.
    ::can_load checks whether the given path could be used in ::load.
    Technically ::can_load is even a bit stricter than ::load, so ::load
    might be able to load a image event if ::can_load returned false.
    
    Theres also a internal function to actually load the image buffer into
    the struct. This isn't done during the load function for performance
    reason but just before the acutal image modifications or operations are
    being executed.

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -1,7 +1,17 @@
-use image::DynamicImage;
-use crate::generic::GenericThumbnail;
-use std::path::Path;
-use crate::thumbnail::operations::Operation;
+use image::{DynamicImage, image_dimensions, ImageResult, ImageError, io::Reader, GenericImageView};
+use crate::{
+    generic::{GenericThumbnail, Crop, Exif, Resize, BoxPosition, Orientation, ResampleFilter},
+    thumbnail::operations::{Operation, ResizeOp},
+    errors::{FileNotFoundError, FileNotSupportedError, FileError, InternalError},
+    errors
+};
+use std::{
+    io,
+    path::Path,
+    error::Error,
+    io::{Read, BufReader},
+    fs::File
+};
 
 mod operations;
 
@@ -21,10 +31,83 @@ pub trait SingleThumbnail : GenericThumbnail {
 
 pub struct Thumbnail<'a> {
     path: &'a Path,
+    reader: Reader<BufReader<File>>,
     height: u32,
     width: u32,
     image: Option<DynamicImage>,
     ops: Vec<&'a dyn Operation>
+}
+
+impl Thumbnail<'_> {
+    pub fn load(path: &Path) -> Result<Thumbnail, FileError> {
+        if !path.is_file() {
+            return Err(FileError::NotFound(FileNotFoundError {
+                path,
+            }));
+        }
+
+        // This unfortunately needs to be mutable, because we may need to overwrite it with itself,
+        // because a method call borrows self and then returns it again within a Result
+        let mut reader = match Reader::open(path) {
+            Ok(reader) => reader,
+            Err(error) => return Err(FileError::IoError(error)),
+        };
+
+        if reader.format().is_none() {
+            // with_guessed_format() returns Result<Self>,
+            // to keep ownership of reader we need to extract it from the result again
+            reader = match reader.with_guessed_format() {
+                Err(error) => return return Err(FileError::IoError(error)),
+                Ok(reader) => reader
+            };
+
+            if reader.format().is_none(){
+                return Err(FileError::NotSupported(FileNotSupportedError {
+                    path,
+                }));
+            }
+        }
+
+        Ok(Thumbnail {
+            path,
+            reader,
+            height: 0,
+            width: 0,
+            image: None,
+            ops: vec![]
+        })
+    }
+
+    pub fn can_load(path: &Path) -> bool {
+        if !path.is_file() {
+            return false;
+        }
+
+        match Reader::open(path) {
+            Err(_) => return false,
+            Ok(reader) => match reader.format() {
+                Some(_) => true,
+                None => false
+            },
+        }
+    }
+
+    fn get_dyn_image<'a>(mut self) -> Result<image::DynamicImage, InternalError<'a>> {
+        if self.image.is_none() {
+            let image = self.reader.decode()?;
+            let (width, height) = match self.image {
+                Some(i) => i.dimensions(),
+                None => (0, 0)
+            };
+            self.width = width;
+            self.height = height;
+            self.image = Option::from(image.clone());
+        }
+
+        let image = self.image.ok_or_else(|| InternalError::UnknownError(errors::UnknownError))?;
+
+        Ok(image)
+    }
 }
 
 
