dced81876816c8bbd9a9619b10cecef8db115646
phillipmlk <phillip_mielke@t-online.de>
2020-05-08 12:41:03 +0200

    Documentation for enums in generic.rs

--- src/generic.rs
+++ src/generic.rs
@@ -3,33 +3,75 @@ use crate::StaticThumbnail;
 #[derive(Debug, Copy, Clone)]
 /// The different options for the resize-operation as an enum
 pub enum Resize {
-    /// Option: scale to a given height, keep aspect ratio
+    /// Option: scale to a given height, keep aspect ratio.
+    /// ### Arguments:
+    /// * height: u32
     Height(u32),
-    /// Option: scale to a given width, keep aspect ratio
+    /// Option: scale to a given width, keep aspect ratio.
+    /// ### Arguments:
+    /// * width: u32
     Width(u32),
-    /// Option: scale the image so that it fits inside the box given by width and height, keep aspect ratio
+    /// Option: scale the image so that it fits inside the box given by width and height, keep aspect ratio.
+    /// ### Arguments:
+    /// * width: u32
+    /// * height: u32
     BoundingBox(u32, u32),
-    /// Option: scale the image to the given width and height exactly, aspect ratio may be changed
+    /// Option: scale the image to the given width and height exactly, aspect ratio may be changed.
+    /// ### Arguments:
+    /// * width: u32
+    /// * height: u32
     ExactBox(u32, u32),
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Different positioning-options for overlays as an enum
 pub enum BoxPosition {
+    /// Coordinates of the top-left-corner in the background image of the overlayed object.
+    /// ### Arguments:
+    /// * position_x: u32
+    /// * position_y: u32
     TopLeft(u32, u32),
+    /// Coordinates of the top-right-corner in the background image of the overlayed object.
+    /// ### Arguments:
+    /// * position_x: u32
+    /// * position_y: u32
     TopRight(u32, u32),
+    /// Coordinates of the bottom-left-corner in the background image of the overlayed object.
+    /// ### Arguments:
+    /// * position_x: u32
+    /// * position_y: u32
     BottomLeft(u32, u32),
+    /// Coordinates of the bottom-right-corner in the background image of the overlayed object.
+    /// ### Arguments:
+    /// * position_x: u32
+    /// * position_y: u32
     BottomRight(u32, u32),
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Different options for cropping as an enum
 pub enum Crop {
+    /// Options for exactly cropping the image to a rectangle given by the coordinates of the top-left-corner and width and height.
+    /// ### Arguments:
+    /// * position_x: u32
+    /// * position_y: u32
+    /// * width: u32
+    /// * height: u32
     Box(u32, u32, u32, u32),
+    /// Option for cropping the image to a rectangle given by a ratio of width and height.
+    /// The rectangle is scaled to the maximum that fits inside the origin image.
+    /// ### Arguments:
+    /// * ratio_width: u32
+    /// * ratio_height: u32
     Ratio(f32, f32),
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Orientation options as an enum
 pub enum Orientation {
+    /// Option for a vertical orientation
     Vertical,
+    /// Option for a horizontal orientation
     Horizontal,
 }
 
@@ -42,11 +84,17 @@ pub enum Exif {
 }
 
 #[derive(Debug, Copy, Clone)]
+/// Collection of filters that can be applied to images
 pub enum ResampleFilter {
+    /// Nearest Neighbor Filter
     Nearest,
+    /// Linear Filter
     Triangle,
+    /// Cubic Filter
     CatmullRom,
+    /// Gaussian Filter
     Gaussian,
+    /// Lanczos with window 3
     Lanczos3,
 }
 
