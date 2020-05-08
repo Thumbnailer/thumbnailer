28f6dbd27c548556f4998478f8ecae82d300e8a6
phillipmlk <phillip_mielke@t-online.de>
2020-05-08 18:25:33 +0200

    Doc for CombineOp

--- src/thumbnail/operations.rs
+++ src/thumbnail/operations.rs
@@ -534,10 +534,10 @@ impl Operation for TextOp {
     /// Logic for the operation of drawing texts on an image
     ///
     /// This function draws a `String` in a `DynamicImage` at the position defined in the `BoxPosition`-enum:
-    /// * with `BoxPosition::TopLeft`: The top-left-corner of the text ist placed at the defined coordinates
-    /// * with `BoxPosition::TopRight`: The top-right-corner of the text ist placed at the defined coordinates
-    /// * with `BoxPosition::BottomLeft`: The bottom-left-corner of the text ist placed at the defined coordinates
-    /// * with `BoxPosition::BottomRight`: The bottom-right-corner of the text ist placed at the defined coordinates
+    /// * with `BoxPosition::TopLeft`: The top-left-corner of the text is placed at the defined coordinates
+    /// * with `BoxPosition::TopRight`: The top-right-corner of the text is placed at the defined coordinates
+    /// * with `BoxPosition::BottomLeft`: The bottom-left-corner of the text is placed at the defined coordinates
+    /// * with `BoxPosition::BottomRight`: The bottom-right-corner of the text is placed at the defined coordinates
     ///
     /// It returns `true` on success and `false` in case of an error.
     ///
@@ -622,18 +622,60 @@ impl Operation for TextOp {
 }
 
 #[derive(Clone)]
+/// Representation of the combine operation as a struct
 pub struct CombineOp {
+    /// The overlay image as `StaticThumbnail`
     image: StaticThumbnail,
+    /// Specifies the position of the Text, represented by `BoxPosition` enum
     pos: BoxPosition,
 }
 
 impl<'a> CombineOp {
+    /// Returns a new `CombineOp` struct with defined:
+    /// * `image` as the image that should be drawn on the 'DynamicImage'
+    /// * `pos` as the position of the text represented by `BoxPosition` enum
     pub fn new(image: StaticThumbnail, pos: BoxPosition) -> Self {
         CombineOp { image, pos }
     }
 }
 
 impl Operation for CombineOp {
+    /// Logic for the operation of drawing an image on top of another image
+    ///
+    /// This function draws a `StaticThumbnail` on top of a `DynamicImage` at the position defined in the `BoxPosition`-enum:
+    /// * with `BoxPosition::TopLeft`: The top-left-corner of the overlayed image is placed at the defined coordinates
+    /// * with `BoxPosition::TopRight`: The top-right-corner of the overlayed image is placed at the defined coordinates
+    /// * with `BoxPosition::BottomLeft`: The bottom-left-corner of the overlayed image is placed at the defined coordinates
+    /// * with `BoxPosition::BottomRight`: The bottom-right-corner of the overlayed image is placed at the defined coordinates
+    ///
+    /// It returns `true` on success and `false` in case of an error.
+    ///
+    /// # Arguments
+    ///
+    /// * `&self` - The `CombineOp` struct
+    /// * `image` - The `DynamicImage` where the text should be drawn on
+    ///
+    /// # Panic
+    ///
+    /// This function won't panic ?
+    ///
+    /// # Examples
+    /// ```
+    /// use thumbnailer::generic::BoxPosition;
+    /// use thumbnailer::thumbnail::StaticThumbnail;
+    /// use thumbnailer::thumbnail::operations::Operation;
+    /// use thumbnailer::thumbnail::operations::CombineOp;
+    /// use image::DynamicImage;
+    ///
+    /// let position = BoxPosition::TopLeft(23, 40);
+    /// let mut dynamic_image = DynamicImage::new_rgb8(800, 500);/*
+    /// let mut static_image = StaticThumbnail{
+    ///     image: DynamicImage::new_rgb8(100, 100),
+    /// };
+    ///
+    /// let combine_op = CombineOp::new(static_image, position);
+    /// combine_op.apply(&mut dynamic_image);*/
+    /// ```
     fn apply(&self, image: &mut DynamicImage) -> bool
     where
         Self: Sized,
